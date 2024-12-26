557a3aad9d072fbf3815fdb5d06083bd1135f644
Kota-Arai <k.arai@slj.jp>
2024-12-26 15:13:07 +0900

    ホームページから書くページへのリンク設定, HTML CSS 配置

--- gym/views.py
+++ gym/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 
 # 簡単なビューを作成
 def home(request):
-    return HttpResponse("こんにちはともやです!!")
+    return render(request, 'gym/home.html') # gym/home.htmlを返す (ホームページ用テンプレートをレンダリング)
+
+def about(request):
+    return render(request, 'gym/about.html') # gym/about.htmlを返す (アバウトページ用テンプレートをレンダリング)
+
+def contact(request):  
+    return render(request, 'gym/contact.html') # gym/contact.htmlを返す (コンタクトページ用テンプレートをレンダリング)
