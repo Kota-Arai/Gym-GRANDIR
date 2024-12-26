94e49cf36bac1d0a5e4fe69f633403fb36661272
Kota-Arai <k.arai@slj.jp>
2024-12-26 19:41:16 +0900

    renderの修正 ページエラー解消

--- gym/views.py
+++ gym/views.py
@@ -1,11 +1,12 @@
+from django.shortcuts import render  # renderをインポート
 from django.http import HttpResponse
 
 # 簡単なビューを作成
 def home(request):
-    return render(request, 'gym/home.html') # gym/home.htmlを返す (ホームページ用テンプレートをレンダリング)
+    return render(request, 'gym/home.html')  # gym/home.htmlを返す (ホームページ用テンプレートをレンダリング)
 
 def about(request):
-    return render(request, 'gym/about.html') # gym/about.htmlを返す (アバウトページ用テンプレートをレンダリング)
+    return render(request, 'gym/about.html')  # gym/about.htmlを返す (アバウトページ用テンプレートをレンダリング)
 
 def contact(request):  
-    return render(request, 'gym/contact.html') # gym/contact.htmlを返す (コンタクトページ用テンプレートをレンダリング)
+    return render(request, 'gym/contact.html')  # gym/contact.htmlを返す (コンタクトページ用テンプレートをレンダリング)
